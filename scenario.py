6b12610a7f472fa31b26964e5423ef8584eed011
Gabriel Mendonça <gabrielgmendonca@gmail.com>
2011-04-24 12:17:54 -0300

    Created Scenario class.

---
+++ scenario.py
@@ -0,0 +1,11 @@
+from panda3d.core import PandaNode
+from panda3d.core import NodePath
+
+class Scenario(NodePath):
+    def __init__(self, parent, model):
+        NodePath.__init__(self, PandaNode("scenario"))
+        
+        self.model = loader.loadModel(model)
+        self.model.reparentTo(self)
+        
+        self.reparentTo(parent)
