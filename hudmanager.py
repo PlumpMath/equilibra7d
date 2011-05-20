2d5db850e35a23589f2ce97d7e868143fc512b7a
Rodolfo Carvalho <rhcarvalho@gmail.com>
2011-05-19 22:42:53 -0300

    Translate HUD text to Portuguese

--- hudmanager.py
+++ hudmanager.py
@@ -1,14 +1,19 @@
 from panda3d.core import TextNode
-
 from direct.gui.OnscreenText import OnscreenText
+
 
 class HUDManager():
     def __init__(self):
-        text = "Commands:\n\n    WASD - Character steering\n    ESC - Exit"
+        text = """\
+Comandos:
+    WASD - Movimentar jogador
+    ESC - Exit
+"""
         pos = (-1, -0.6)
         scale = 0.06
         align = TextNode.ALeft
         fg = (1, 1, 1, 1)
         
-        textNode = OnscreenText(text=text, pos=pos, scale=scale, align=align, 
-                                fg=fg)
+        textNode = OnscreenText(text=text, pos=pos, scale=scale, 
+                                align=align, fg=fg)
+
