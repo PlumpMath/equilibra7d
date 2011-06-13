7056097f37c98408f98761446ef302debc47976a
Gabriel Mendonça <gabrielgmendonca@gmail.com>
2011-06-13 13:37:09 -0300

    Improves character keyboard control.

--- objects/character.py
+++ objects/character.py
@@ -17,37 +17,39 @@ class Character(PhysicalNode, CollisionEventHandler, KeyboardEventHandler):
         
         self.keys = dict.fromkeys("left right up down".split(), 0)
         self.bindings = (
-            ("w", self.handleKeyboardEvent, ["up", 1]),
-            ("a", self.handleKeyboardEvent, ["left", 1]),
-            ("s", self.handleKeyboardEvent, ["down", 1]),
-            ("d", self.handleKeyboardEvent, ["right", 1]),
+            ("w", self.setKey, ["up", 1]),
+            ("a", self.setKey, ["left", 1]),
+            ("s", self.setKey, ["down", 1]),
+            ("d", self.setKey, ["right", 1]),
 
-            ("w-up", self.handleKeyboardEvent, ["up", 0]),
-            ("a-up", self.handleKeyboardEvent, ["left", 0]),
-            ("s-up", self.handleKeyboardEvent, ["down", 0]),
-            ("d-up", self.handleKeyboardEvent, ["right", 0]),
+            ("w-up", self.setKey, ["up", 0]),
+            ("a-up", self.setKey, ["left", 0]),
+            ("s-up", self.setKey, ["down", 0]),
+            ("d-up", self.setKey, ["right", 0]),
             
-            ("arrow_up", self.handleKeyboardEvent, ["up", 1]),
-            ("arrow_left", self.handleKeyboardEvent, ["left", 1]),
-            ("arrow_down", self.handleKeyboardEvent, ["down", 1]),
-            ("arrow_right", self.handleKeyboardEvent, ["right", 1]),
+            ("arrow_up", self.setKey, ["up", 1]),
+            ("arrow_left", self.setKey, ["left", 1]),
+            ("arrow_down", self.setKey, ["down", 1]),
+            ("arrow_right", self.setKey, ["right", 1]),
 
-            ("arrow_up-up", self.handleKeyboardEvent, ["up", 0]),
-            ("arrow_left-up", self.handleKeyboardEvent, ["left", 0]),
-            ("arrow_down-up", self.handleKeyboardEvent, ["down", 0]),
-            ("arrow_right-up", self.handleKeyboardEvent, ["right", 0]),
+            ("arrow_up-up", self.setKey, ["up", 0]),
+            ("arrow_left-up", self.setKey, ["left", 0]),
+            ("arrow_down-up", self.setKey, ["down", 0]),
+            ("arrow_right-up", self.setKey, ["right", 0]),
             
             # These are fired when the user keeps pressing a key
-            ("w-repeat", self.handleKeyboardEvent, ["up", 1]),
-            ("a-repeat", self.handleKeyboardEvent, ["left", 1]),
-            ("s-repeat", self.handleKeyboardEvent, ["down", 1]),
-            ("d-repeat", self.handleKeyboardEvent, ["right", 1]),
-            ("arrow_up-repeat", self.handleKeyboardEvent, ["up", 1]),
-            ("arrow_left-repeat", self.handleKeyboardEvent, ["left", 1]),
-            ("arrow_down-repeat", self.handleKeyboardEvent, ["down", 1]),
-            ("arrow_right-repeat", self.handleKeyboardEvent, ["right", 1]),
+            #("w-repeat", self.handleKeyboardEvent, ["up", 1]),
+            #("a-repeat", self.handleKeyboardEvent, ["left", 1]),
+            #("s-repeat", self.handleKeyboardEvent, ["down", 1]),
+            #("d-repeat", self.handleKeyboardEvent, ["right", 1]),
+            #("arrow_up-repeat", self.handleKeyboardEvent, ["up", 1]),
+            #("arrow_left-repeat", self.handleKeyboardEvent, ["left", 1]),
+            #("arrow_down-repeat", self.handleKeyboardEvent, ["down", 1]),
+            #("arrow_right-repeat", self.handleKeyboardEvent, ["right", 1]),
         )
     
+        taskMgr.add(self.handleKeyboardEvent, "character_move_task")
+        
     def setup(self):
         self.setZ(5)
         self.setScale(0.8)
@@ -56,11 +58,10 @@ class Character(PhysicalNode, CollisionEventHandler, KeyboardEventHandler):
         normal = entry.getSurfaceNormal(self)
         self.addImpulse(normal * self._impact)
     
-    def handleKeyboardEvent(self, key, value):
+    def handleKeyboardEvent(self, task):
         keys = self.keys
-        keys[key] = value
         
-        dt = 0.05
+        dt = globalClock.getDt()
         impulse = Vec3(0, 0, 0)
         increment = self._impulseIncrement * dt
         
@@ -75,6 +76,11 @@ class Character(PhysicalNode, CollisionEventHandler, KeyboardEventHandler):
         
         self.addImpulse(impulse)
         self.face(self.getVelocity())
+        
+        return task.cont
+    
+    def setKey(self, key, value):
+        self.keys[key] = value
     
     def is_braking(self, coordinate):
         velocity = self.getVelocity()
