10d8ae1beda4a74d5c472a8f34a76398d0617ad6
Danielle Caled <danikhaled@gmail.com>
2011-06-21 19:15:44 -0300

    Changes in the main character.

--- equilibra7d.py
+++ equilibra7d.py
@@ -23,7 +23,7 @@ class World(ShowBase):
         if len(sys.argv) == 2:
             model = sys.argv[1]
         else:
-            model = "character_1_4"
+            model = "character_4_1"
         self.character = Character(self.render, model)
         self.enemy = Enemy(self.render, "enemy")
         self.landscape = Landscape(self.render, "landscape")
@@ -72,4 +72,6 @@ class World(ShowBase):
 if __name__ == "__main__":
     world = World()
     world.run()
+
+
 
