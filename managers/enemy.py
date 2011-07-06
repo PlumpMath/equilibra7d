b5f87b07378bd9156a334085c6cc21e05e322a82
Gabriel Mendonça <gabrielgmendonca@gmail.com>
2011-07-06 17:57:21 -0300

    Adds EnemyManager class.

---
+++ managers/enemy.py
@@ -0,0 +1,14 @@
+from base import Manager
+
+
+class EnemyManager(Manager):
+    """Handles the dynamic creation and destruction of Enemy objects."""
+
+    def __init__(self):
+        pass
+    
+    def setup(self):
+        pass
+    
+    def clear(self):
+        pass
