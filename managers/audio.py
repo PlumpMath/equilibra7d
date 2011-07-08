43ad4f4b956e8bbe830b30b0c026643e29185d0f
Gabriel Mendonça <gabrielgmendonca@gmail.com>
2011-07-08 12:54:01 -0300

    Adds AudioManager class.

---
+++ managers/audio.py
@@ -0,0 +1,14 @@
+from base import Manager
+
+
+class AudioManager(Manager):
+    """Handles game's sound effects and music."""
+    
+    def __init__(self):
+        pass
+    
+    def setup(self):
+        pass
+        
+    def clear(self):
+        pass
